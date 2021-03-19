6f733e8a5c70d021354a76131a756a8aed541b93
Sophie-droid <Sophie-droid.github.io>
2021-03-19 15:41:12 +0100

    mise en ligne

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Entity\User;
+use App\Form\RegistrationType;
+
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+   * @Route("/inscription", name="security_registration")
+   */
+  public function registration(Request $request, EntityManagerInterface $entityManager, UserPasswordEncoderInterface $encoder)
+  {
+    $user = new User();
+
+    $form = $this->createForm(RegistrationType::class, $user);
+
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      $hash = $encoder->encodePassword($user, $user->getPassword());
+
+      $user->setPassword($hash);
+
+      $entityManager->persist($user);
+      $entityManager->flush();
+
+      if ($user->getId()) {
+        $this->addFlash('success', 'Votre compte a bien été créé.');
+      } else {
+        $this->addFlash('error', 'Votre compte n\'a pas pu être créé.');
+      }
+
+      return $this->redirectToRoute('app_login');
+    }
+
+    return $this->render('security/registration.html.twig', [
+      'form' => $form->createView(),
+    ]);
+  }
+}
